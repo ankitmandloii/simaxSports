5549f007e215464d3ce9094a2a3e6765f17d872e
Vaishaliitgeek <V@i$h@li1234>
2025-05-01 12:35:40 +0530

    ad product toolbar done

--- frontend/src/components/Toolbar/ProductToolbar/ProductToolbar.jsx
+++ frontend/src/components/Toolbar/ProductToolbar/ProductToolbar.jsx
@@ -1,39 +1,46 @@
 import React from 'react'
 import './ProductToolbar.css'
-import camera from '../../images/camera.jpg'
 import { IoAdd } from "react-icons/io5";
+import miniProd from '../../images/mini-prod.png'
+import colorwheel from '../../images/color-wheel.png'
 const ProductToolbar = () => {
   return (
     <div className="toolbar-main-container">
 <div className='toolbar-main-heading'>
   <h5 className='Toolbar-badge'>Product</h5>
-  <h3 >Manage Your Products</h3>
+  <h2 >Manage Your Products</h2>
   <p>You can select multiple products and colors</p>
 </div>
 <div className="toolbar-box">
   <div className="toolbar-box-top-content">
   <div className="toolbar-head">
-    <img src={camera} className='product-mini-img'/>
+    <div className="mini-prod-img-container">
+    <img src={miniProd} className='product-mini-img'/>
+
+    </div>
     <div >
- <h3>Essential red tshirt for men and women</h3>
- <p>Change</p>
+ <h4>Essential red tshirt for men and women</h4>
+ <p className='toolbar-span'>Change</p>
     </div>
   </div>
-  <div className='flex  '>
+  <div className='toolbar-middle-button '>
     <button className='black-button'>Change Product</button>
-    <button>Add Cart</button>
+    <div className='addCart-button'>
+      <img src={colorwheel} alt="image" className='color-img'/>
+       <p>Add Color</p>
+    </div>
   </div>
   </div>
   
   <button className='add-product-btn'> <IoAdd/> Add Products</button>
-  <p>Customize method Example</p>
-  <div className='common-btn'>
-    <p>Printing</p>
+  <p className='center'>Customize Method Example</p>
+  <div className='common-btn active'>
+    <h4>Printing</h4>
     <p>No minimum</p>
 
   </div>
   <div className='common-btn'>
-    <p>Printing</p>
+    <h4>Ebbroidery</h4>
     <p>No minimum</p>
 
   </div>
