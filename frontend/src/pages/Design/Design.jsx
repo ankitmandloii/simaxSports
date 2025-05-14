55e5ccbaec8b719a85a8a2bb60cf408f99292ebf
ankitmandloii <ankitmandloi@itgeeks.com>
2025-05-14 11:54:21 +0530

    update code

--- frontend/src/pages/Design/Design.jsx
+++ frontend/src/pages/Design/Design.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Design.css'
 import AppRoutes from '../../routes/Approutes'
 import ProductContainer from '../../components/ProductContainer'
+
 const Design = () => {
   return (
     <div className='layout-container flex gap-5'>
