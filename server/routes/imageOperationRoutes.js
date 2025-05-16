e17e7f38722068143d41856eda27207432e9d1ba
ankitmandloii <162083873+ankitmandloii@users.noreply.github.com>
2025-05-16 12:43:37 +0530

    Update imageOperationRoutes.js

--- server/routes/imageOperationRoutes.js
+++ server/routes/imageOperationRoutes.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const controllers = require("../controller/imageOperations.controller.js");
-const multer = require('multer');
-const router = express.Router();
+// const express = require("express");
+// const controllers = require("../controller/imageOperations.controller.js");
+// const multer = require('multer');
+// const router = express.Router();
 
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-router.post("/convertToPng",  upload.single('image'),controllers.convertToPng);
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage });
+// router.post("/convertToPng",  upload.single('image'),controllers.convertToPng);
 
 
-module.exports = router;
+// module.exports = router;
