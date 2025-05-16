fdbf31594ce85bf950bcf1154a21f09e621a1350
ankitmandloii <162083873+ankitmandloii@users.noreply.github.com>
2025-05-16 12:44:32 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const authRoutes = require("./authRoute.js");
 const productsRoutes = require("./productRoute.js");
-const imageOperationRoutes = require("./imageOperationRoutes.js");
+// const imageOperationRoutes = require("./imageOperationRoutes.js");
 
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/products", productsRoutes);
-router.use("/imageOperation",imageOperationRoutes)
+// router.use("/imageOperation",imageOperationRoutes)
 
 
 
