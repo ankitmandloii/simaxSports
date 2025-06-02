23ed17e595927f79bc2bd4ee800f17542ec3819a
ankitmandloii <ankitmandloi@itgeeks.com>
2025-06-02 12:27:46 +0530

    update API CHange password

--- admin-panel/src/admin/settings/AccountSettings.jsx
+++ admin-panel/src/admin/settings/AccountSettings.jsx
@@ -19,14 +19,79 @@ export default function AccountSettings() {
   const { showToast } = useToast();
 
   const [AdminName, setAdminName] = useState('admin');
-  const [email, setEmail] = useState('admin@simaxapparel.com');
+  const [email, setEmail] = useState('adminsimax@yopmail.com');
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [newPasswordError, setNewPasswordError] = useState('');
 
 
-  const SettingsAdminInfosaved = () => {
-    console.log('Settings saved:', { AdminName, email, oldPassword, newPassword });
-   showToast({ content: 'Settings updated successfully!' });
+
+  const validateEmail = useCallback((value) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!value) return 'Email is required';
+    else if (!emailRegex.test(value)) return 'Please enter a valid email address';
+    return '';
+  }, []);
+
+  const validatePassword = useCallback((value) => {
+    if (!value) return 'Password is required';
+    else if (value.length < 5) return 'Password must be at least 5 characters long';
+    return '';
+  }, []);
+
+
+  const ChangePasswordAdmin = async () => {
+    try {
+      setLoading(true);
+
+      const emailValidation = validateEmail(email);
+      const passwordValidation = validatePassword(oldPassword);
+      const newPasswordValidation = validatePassword(newPassword)
+
+
+      setEmailError(emailValidation);
+      setPasswordError(passwordValidation);
+      setNewPasswordError(newPasswordValidation);
+
+      if (emailValidation || passwordValidation || newPasswordValidation) return;
+
+      const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+
+
+      const token = localStorage.getItem('admin-token');
+
+
+      const response = await fetch(`${BASE_URL}/auth/admin-change-password`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, oldPassword, newPassword }),
+      });
+
+      const data = await response.json();
+
+
+      if (!response.ok) throw new Error(data?.message || 'Something Went Wrong');
+
+      showToast({ content: `${data.message}` });
+
+    } catch (error) {
+
+      showToast({ content: `${error.message}`, error: true });
+    } finally {
+      setLoading(false);
+    }
+
+
+
+
+
   };
 
   return (
@@ -50,6 +115,7 @@ export default function AccountSettings() {
               value={email}
               onChange={setEmail}
               autoComplete="email"
+              error={emailError}
             />
           </Box>
           <Box paddingBlockStart="300">
@@ -58,6 +124,7 @@ export default function AccountSettings() {
               value={oldPassword}
               onChange={setOldPassword}
               autoComplete="tel"
+              error={passwordError}
             />
           </Box>
           <Box paddingBlockStart="300">
@@ -66,10 +133,11 @@ export default function AccountSettings() {
               value={newPassword}
               onChange={setNewPassword}
               autoComplete="tel"
+              error={newPasswordError}
             />
           </Box>
           <InlineStack align="end" paddingBlockStart="300">
-            <Button variant="primary" onClick={SettingsAdminInfosaved}>
+            <Button variant="primary" onClick={ChangePasswordAdmin} loading={loading}>
               Save Admin Info
             </Button>
           </InlineStack>
