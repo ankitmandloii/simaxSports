bbcd30422473caa42ce8a393e02096bedc9c8c08
ankitmandloii <ankitmandloi@itgeeks.com>
2025-06-04 14:14:40 +0530

    Update and create getOrderList

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const authRoutes = require("./authRoute.js");
 const productsRoutes = require("./productRoute.js");
 const designRoutes = require("./designRoute.js");
+const customerRoutes = require("./customerRoutes.js");
 // const imageOperationRoutes = require("./imageOperationRoutes.js");
 
 
@@ -11,6 +12,7 @@ const router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/products", productsRoutes);
 router.use("/design", designRoutes);
+router.use("/customer",customerRoutes)
 // router.use("/imageOperation",imageOperationRoutes)
 
 
