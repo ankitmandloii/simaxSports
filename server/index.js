e2ec85090b43fc67319faac415fc2566e0f36b6c
ankitmandloii <ankitmandloi@itgeeks.com>
2025-06-05 16:10:28 +0530

    update names and number screen reolve

--- server/index.js
+++ server/index.js
@@ -3,14 +3,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-
-
 const PORT = process.env.SERVER_PORT || 8080;
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const routes = require("./routes/index.js");
 const { dbConnection } = require('./config/db');
 const { initSocket } = require('./socket'); // adjust path
+
+
+//for real sync
 initSocket(server);
 
 
@@ -28,12 +29,16 @@ app.use("/api", routes);
 
 // Test route
 app.use("/", (req, res) => {
-  res.send("Yes, now you hit APIs");
+  res.send("Yes, Now you can hit APIs");
 });
 
 
 
 // Start the server
-server.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log(`Someting Went Wrong in Server, Error is  ${err}`);
+  } else {
+    console.log(`Server is ready to listen on port ${PORT}`);
+  }
 });
