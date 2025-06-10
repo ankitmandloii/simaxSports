1ff181a1887f51240cff0e3674fb15d26aad85f1
ankitmandloii <ankitmandloi@itgeeks.com>
2025-06-10 17:40:35 +0530

    resolve bug in login flow

--- admin-panel/src/admin/Login.jsx
+++ admin-panel/src/admin/Login.jsx
@@ -57,7 +57,7 @@ export default function Login() {
             const response = await fetch(`${BASE_URL}auth/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, password }),
+                body: JSON.stringify({ email, password, role: "admin" }),
             });
 
             const data = await response.json();
