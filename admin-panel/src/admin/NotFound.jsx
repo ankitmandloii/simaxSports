2cce6fb5242b5e24d30112ba09bf2a9023d0a83c
ankitmandloii <ankitmandloi@itgeeks.com>
2025-06-10 13:16:44 +0530

    reolve bug relted to GenralSettingsGet

--- admin-panel/src/admin/NotFound.jsx
+++ admin-panel/src/admin/NotFound.jsx
@@ -1,7 +1,6 @@
 // src/admin/NotFound.js
 import { Page, Text, Button, Card, Box } from "@shopify/polaris";
 import { IconsIcon } from "@shopify/polaris-icons";
-
 import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
@@ -13,20 +12,26 @@ export default function NotFound() {
 
   return (
     <Page fullWidth>
-      <Box padding="8" background="bg-subdued" borderRadius="2xl">
+      <Box
+        minHeight="80vh" // make sure it fills most of the page
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        background="bg-subdued"
+        padding="8"
+        borderRadius="2xl"
+      >
         <Card>
           <Box padding="8" textAlign="center">
-          
-              <Text variant="heading2xl" as="h1" tone="critical">
-                404 - Page Not Found
-              </Text>
-              <Text variant="bodyLg">
-                Oops! The page you're looking for doesn't exist or has been moved.
-              </Text>
-              <Button icon={IconsIcon} onClick={handleGoHome} primary>
-                Go to Dashboard
-              </Button>
-            
+            <Text variant="heading2xl" as="h1" tone="critical">
+              404 - Page Not Found
+            </Text>
+            <Text variant="bodyLg" tone="subdued" as="p" paddingBlockStart="4" paddingBlockEnd="6">
+              Oops! The page you're looking for doesn't exist or has been moved.
+            </Text>
+            <Button icon={IconsIcon} onClick={handleGoHome} primary>
+              Go to Dashboard
+            </Button>
           </Box>
         </Card>
       </Box>
