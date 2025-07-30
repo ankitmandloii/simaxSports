83a6142c36b6ba4377e4faae4189d76b8384bda7
ankitmandloii <ankitmandloi@itgeeks.com>
2025-07-30 11:10:30 +0530

    update product lits code and product by collection is and add code of static varint add

---
+++ server/model/ProductVariantSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ProductVariantSchema = new mongoose.Schema({
+  product_id: { type: Number, required: true },
+  variant_id: { type: Number, required: true },
+  sku: { type: String, required: true },
+  price: { type: Number, required: true },
+  inventory_quantity: { type: Number, required: true },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  image_url: { type: String },
+});
+
+const ProductVariant = mongoose.model('ProductVariantSchema', ProductVariantSchema);
+
+module.exports = ProductVariant;
