de9695d29b1dbcc4bfa5b3f2dcc1421c9acd9919
ankitmandloii <ankitmandloi@itgeeks.com>
2025-08-18 13:19:56 +0530

    Update Ui for admin dashboard UI

--- admin-panel/src/admin/Dashboard.jsx
+++ admin-panel/src/admin/Dashboard.jsx
@@ -15,6 +15,8 @@ import {
   Tabs,
   Divider,
   Box,
+  LegacyCard,
+  EmptyState,
 } from "@shopify/polaris";
 
 export function Dashboard() {
@@ -76,9 +78,19 @@ export function Dashboard() {
             <Divider />
             <Box padding="400">
               {userDesigns.length === 0 ? (
-                <Text variant="bodySm" tone="subdued">
-                  No designs found.
-                </Text>
+                // <LegacyCard sectioned>
+                <EmptyState
+                  heading=" No designs found."
+                  // action={{content: 'Add transfer'}}
+                  // secondaryAction={{
+                  //   content: 'Learn more',
+                  //   url: 'https://help.shopify.com',
+                  // }}
+                  image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+                >
+                  <p>Track and receive your incoming designs from customers.</p>
+                </EmptyState>
+                // </LegacyCard>
               ) : (
                 <ResourceList
                   items={userDesigns}
