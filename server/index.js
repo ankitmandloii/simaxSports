7ceb70fbba3e3c6767ebdefd3d88c97b60b4e123
ankitmandloii <ankitmandloi@itgeeks.com>
2025-09-03 12:17:39 +0530

    update patch

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
   origin: process.env.CORS_ORIGIN || '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 }));
 
